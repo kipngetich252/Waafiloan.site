3cef4ed6b9f89874da3200b94c63c02cfaf02024
kipngetich252 <waafiaronbot2@gmail.com>
2026-05-21 18:19:44 +0300

    Create Waafiaron.js

---
+++ Waafiaron.js
@@ -0,0 +1,13 @@
+{
+  "name": "waafiloan-site",
+  "version": "1.0.0",
+  "main": "Waafiloan.js",
+  "scripts": {
+    "start": "node Waafiloan.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "telegraf": "^4.16.0",
+    "dotenv": "^16.3.1"
+  }
+}
